96c9c9bb848a0fec565ffc826003a7eef5dfccd2
SakibHasanMD <114472984+SakibHasanMD@users.noreply.github.com>
2022-09-27 22:27:46 +0600

    Update task4.cpp
    Fixed task 4

--- task4.cpp
+++ task4.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 
 int main()
-{int search_number,i,duplicate_num;
+{int search_number,i,duplicate_num=0;
     int A[10]={8,4,6,1,6,9,6,1,9,8};
-    cout<<"Input a number to search:";
+
+    cout<<"Input a number to search: ";
     cin>>search_number;
-
+//comparing all elements of A to search_number
     for(i=0;i<10;i++){
-
-        if(A[i]==search_number){
+//if any duplicate value is available the duplicate_num increases
+        if(search_number==A[i]){
             duplicate_num++;
 
         }
+
     }
-    cout<<"The Number occurs ";
-    cout<<duplicate_num;
-     cout<< " Times In The Array.";
+    cout<<"The Number occurs "<<duplicate_num<<" Times In The Array.";
 
 }
