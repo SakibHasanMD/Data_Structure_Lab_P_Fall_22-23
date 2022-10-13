76bd062dfa5f8545d97587226a09f78a22a92804
SakibHasanMD <114472984+SakibHasanMD@users.noreply.github.com>
2022-10-13 22:28:03 +0600

    Added lab4 task
    I Could not update in lab class because my GitHub got locked.

---
+++ class and object sorting.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+class student{
+    public:
+string name;
+int id;
+double cgpa;
+};
+int main()
+{
+    student object1;
+    student object2;
+    student object3;
+    student object4;
+    student object5;
+// Name
+    object1.name="Student 1";
+    object1.name="Student 1";
+    object1.name="Student 1";
+    object1.name="Student 1";
+    object1.name="Student 1";
+//ID
+    object1.id=12;
+    object2.id=64;
+    object3.id=36;
+    object4.id=24;
+    object5.id=43;
+//CGPA
+    object1.cgpa=3.5;
+    object2.cgpa=3.6;
+    object3.cgpa=4;
+    object4.cgpa=3.1;
+    object5.cgpa=3.9;
+
+
+
+
+}
